0529e95398383f9e5a453f675bb20770aa019e3a
danieljesusgonz <danieljesusgonz@gmail.com>
2022-09-15 00:25:43 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["Amazin", "angry", "happy", "hungry"];
+  let noun = ["cat", "dog", "car", "phone", "house"];
+  let dotSom = [".com", ".us", ".gov", ".net"];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let ds = 0; ds < dotSom.length; ds++) {
+          console.log(pronoun[p] + adj[a] + noun[n] + dotSom[ds]);
+        }
+      }
+    }
+  }
 };
